b0d5071b006088ee01dab8e22c12d5b4ac5a78f4
Ulises Torner <ulises@Ulisess-iMac.bwrouter>
2021-07-07 01:31:45 -0500

    Update: Authentication, component form styling and markup

--- src/Components/AuthenticationForm.jsx
+++ src/Components/AuthenticationForm.jsx
@@ -1,12 +1,45 @@
-import { bool } from "prop-types";
+import { func } from "prop-types";
 import styles from './AuthenticationForm.module.css';
 
-const AuthenticationForm = () => {
+const AuthenticationForm = ({ handleSubmit }) => {
+  const onSubmit = event => {};
+
   return (
     <>
       <div className={styles.layout}>
         <div className={styles.container}>
-          <h3>Holis</h3>
+          <h3 className={styles.title}>
+            ¡Regístrate!
+          </h3>
+          <form>
+            <div>
+              <label for="username" className={styles.label}>
+                Correo electrónico:
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                className={styles.input}
+              />
+            </div>
+
+            <div>
+              <label for="password" className={styles.label}>
+                Contraseña:
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className={styles.input}
+              />
+            </div>
+
+            <button type="submit" className={styles.submitBtn}>
+              Registrarme
+            </button>
+          </form>
         </div>
       </div>
       <div className={styles.overlay}></div>
@@ -15,9 +48,11 @@ const AuthenticationForm = () => {
 }
 
 AuthenticationForm.propTypes = {
+  handleSubmit: func,
 };
 
 AuthenticationForm.defaultProps = {
+  handleSubmit: () => {},
 };
 
 export default AuthenticationForm;
